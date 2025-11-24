494c791b8ebbfc9c87a78e933b1253e29d4f3fa1
JalenLv <jalenlv543@gmail.com>
2025-11-24 17:53:24 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Beginner-Client-Libraries/Colcon-Tutorial.html - 00

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -e
+
+set +e
+rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y 2>&1 | tee /tmp/rosdep_output.log
+rosdep_exit_code=${PIPESTATUS[0]}
+set -e
+
+if [ $rosdep_exit_code -ne 0 ]; then
+    if grep -q "your rosdep installation has not been initialized yet" /tmp/rosdep_output.log; then
+        echo "rosdep install failed because it is not initialized."
+        if grep -q "sudo rosdep init" /tmp/rosdep_output.log; then
+            read -p "Do you want to run 'sudo rosdep init'? [Y/n] " -r
+            if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+                sudo rosdep init
+            else
+                exit 1
+            fi
+        fi
+        if grep -q "rosdep update" /tmp/rosdep_output.log; then
+            read -p "Do you want to run 'rosdep update'? [Y/n] " -r
+            if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+                rosdep update
+            else
+                exit 1
+            fi
+        fi
+        rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y
+    else
+        exit $rosdep_exit_code
+    fi
+fi
+rm -f /tmp/rosdep_output.log
+
+colcon build --symlink-install --event-handlers console_direct+ --cmake-args \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -GNinja
