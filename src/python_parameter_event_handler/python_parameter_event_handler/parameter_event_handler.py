10969b34d6a9858197d0a174a33b193a1b079015
JalenLv <jalenlv543@gmail.com>
2025-11-25 23:24:10 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Monitoring-For-Parameter-Changes-Python.html - 00

---
+++ src/python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+import rclpy.parameter
+
+from rclpy.parameter_event_handler import ParameterEventHandler
+
+class SampleNodeWithParameters(Node):
+    def __init__(self):
+        super().__init__("node_with_parameters")
+
+        self.declare_parameter("an_int_param", 0)
+
+        self.handler = ParameterEventHandler(self)
+
+        self.callback_handle = self.handler.add_parameter_callback(
+            parameter_name="an_int_param",
+            node_name=self.get_name(),
+            callback=self.callback,
+        )
+
+    def callback(self, p: rclpy.parameter.Parameter) -> None:
+        self.get_logger().info(
+            "Received an update to parameter: {}: {}".format(
+                p.name, rclpy.parameter.parameter_value_to_python(p.value)
+            )
+        )
+
+def main():
+    rclpy.init()
+    node = SampleNodeWithParameters()
+    rclpy.spin(node)
+    rclpy.shutdown()
