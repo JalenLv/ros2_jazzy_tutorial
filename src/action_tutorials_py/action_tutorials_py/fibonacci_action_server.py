6f69809866a9c9f6bd42147e4378de50d39eb72a
JalenLv <jalenlv543@gmail.com>
2025-11-25 19:50:25 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Writing-an-Action-Server-Client/Py.html - 03

--- src/action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
+++ src/action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
@@ -5,6 +5,8 @@ from rclpy.action.server import ServerGoalHandle
 
 from custom_action_interfaces.action import Fibonacci
 
+import time
+
 class FibonacciActionServer(Node):
     def __init__(self):
         super().__init__("fibonacci_action_server")
@@ -18,15 +20,22 @@ class FibonacciActionServer(Node):
     def execute_callback(self, goal_handle: ServerGoalHandle):
         self.get_logger().info("Executing goal...")
 
-        sequence = [0, 1]
+        feedback_msg = Fibonacci.Feedback()
+        feedback_msg.partial_sequence = [0, 1]
+
         request: Fibonacci.Request = goal_handle.request
         for i in range(1, request.order):
-            sequence.append(sequence[i] + sequence[i - 1])
+            feedback_msg.partial_sequence.append(
+                feedback_msg.partial_sequence[i] + feedback_msg.partial_sequence[i - 1]
+            )
+            self.get_logger().info("Feedback: {}".format(feedback_msg.partial_sequence))
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(1)
 
         goal_handle.succeed()
 
         result = Fibonacci.Result()
-        result.sequence = sequence
+        result.sequence = feedback_msg.partial_sequence
         return result
 
 def main():
