d49cdadcfcde7aaad83f79cff2aa905ed2c83b8b
JalenLv <jalenlv543@gmail.com>
2025-11-25 19:36:54 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Writing-an-Action-Server-Client/Py.html - 00

---
+++ src/action_tutorials_py/action_tutorials_py/fibonacci_action_server.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.action import ActionServer
+
+from custom_action_interfaces.action import Fibonacci
+
+class FibonacciActionServer(Node):
+    def __init__(self):
+        super().__init__("fibonacci_action_server")
+        self._action_server = ActionServer(
+            self,
+            Fibonacci,
+            "fibonacci",
+            self.execute_callback
+        )
+    
+    def execute_callback(self, goal_handle):
+        self.get_logger().info("Executing goal...")
+        result = Fibonacci.Result()
+        return result
+
+def main():
+    rclpy.init()
+
+    fibonacci_action_server = FibonacciActionServer()
+    rclpy.spin(fibonacci_action_server)
+
+    fibonacci_action_server.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
