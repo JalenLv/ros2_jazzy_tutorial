0d3e42a0140ff1eb2e0b515aac95c851d3a1a4ab
JalenLv <jalenlv543@gmail.com>
2025-11-25 23:55:28 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Monitoring-For-Parameter-Changes-Python.html - 02

--- src/python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
+++ src/python_parameter_event_handler/python_parameter_event_handler/parameter_event_handler.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 import rclpy.parameter
+from rcl_interfaces.msg import Parameter, ParameterEvent
 
 from rclpy.parameter_event_handler import ParameterEventHandler
 
@@ -9,6 +10,7 @@ class SampleNodeWithParameters(Node):
         super().__init__("node_with_parameters")
 
         self.declare_parameter("an_int_param", 0)
+        self.declare_parameter("another_double_param", 0.0)
 
         self.handler = ParameterEventHandler(self)
 
@@ -24,12 +26,31 @@ class SampleNodeWithParameters(Node):
             self.callback,
         )
 
-    def callback(self, p: rclpy.parameter.Parameter) -> None:
+        self.event_callback_handle = self.handler.add_parameter_event_callback(
+            self.event_callback
+        )
+
+    def callback(self, p: Parameter) -> None:
         self.get_logger().info(
             "Received an update to parameter: {}: {}".format(
                 p.name, rclpy.parameter.parameter_value_to_python(p.value)
             )
         )
+
+    def event_callback(self, parameter_event: ParameterEvent):
+        self.get_logger().info(f"Type: {type(parameter_event)}")
+        self.get_logger().info(f"Type: {type(parameter_event.changed_parameters)}")
+        self.get_logger().info(f"Type: {type(parameter_event.changed_parameters[0])}")
+
+        self.get_logger().info(f"Received parameter event from node {parameter_event.node}")
+
+        p: Parameter
+        for p in parameter_event.changed_parameters:
+            self.get_logger().info(
+                "Inside event: {} changed to: {}".format(
+                    p.name, rclpy.parameter.parameter_value_to_python(p.value)
+                )
+            )
 
 def main():
     rclpy.init()
