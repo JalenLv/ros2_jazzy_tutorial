93cd04ee1cf9ac26093502a06853065b28b75376
JalenLv <jalenlv543@gmail.com>
2025-11-26 09:43:39 -0600

    https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Launch/Using-ROS2-Launch-For-Large-Projects.html - 00

---
+++ src/launch_reuse_tutorial/launch/launch_turtlesim_launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import PushRosNamespace
+
+def generate_launch_description():
+    launch_dir = PathJoinSubstitution(
+        [FindPackageShare("launch_reuse_tutorial"), "launch"]
+    )
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PathJoinSubstitution(
+                [launch_dir, "turtlesim_world_1_launch.py"]
+            ),
+        ),
+        GroupAction(
+            actions=[
+                PushRosNamespace("turtlesim2"),
+                IncludeLaunchDescription(
+                    PathJoinSubstitution(
+                        [launch_dir, "turtlesim_world_1_launch.py"]
+                    ),
+                ),
+            ],
+        ),
+        IncludeLaunchDescription(
+            PathJoinSubstitution(
+                [launch_dir, "broadcaster_listener_launch.py"]
+            ),
+            launch_arguments={
+                "target_frame": "carrot1"
+            }.items(),
+        ),
+        IncludeLaunchDescription(
+            PathJoinSubstitution(
+                [launch_dir, "mimic_launch.py"]
+            ),
+        ),
+        IncludeLaunchDescription(
+            PathJoinSubstitution(
+                [launch_dir, "fixed_broadcaster_launch.py"]
+            ),
+        ),
+        IncludeLaunchDescription(
+            PathJoinSubstitution(
+                [launch_dir, "turtlesim_rviz_launch.py"]
+            ),
+        ),
+    ])
